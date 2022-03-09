b70cf4d07cf3e286d2401cdd5e5c7dec9575aa2a
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-09 17:06:29 +0000

    revalidate a bit like sanity does it

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -2,18 +2,29 @@ import { isValidRequest } from '@sanity/webhook'
 
 const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
 
-export default async function handler(req, res) {
-  // Check for secret to confirm this is a valid request
-  if (req.query.secret !== previewSecret) {
-    return res.status(401).json({ message: 'Invalid token' })
+
+const revalidate = async (req, res) => {
+  if (!isValidRequest(req, previewSecret)) {
+    return res.status(401).json({
+      success: false,
+      message: 'Invalid signature',
+    })
   }
 
   try {
+    // Customise the pages you need to revalidate here.
+    // Keep the is simple for now
     await res.unstable_revalidate('/our-research')
-    return res.json({ revalidated: true })
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error revalidating',
+    })
   }
 }
+
+export default revalidate
