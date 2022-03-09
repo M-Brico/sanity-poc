35396342a656322b48f7b2d0bc915b4422dc31f8
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-09 14:22:14 +0000

    hard code signature

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -1,10 +1,12 @@
 import { isValidRequest } from '@sanity/webhook'
 
+const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
+
 const revalidate = async (req, res) => {
-  if (!isValidRequest(req, process.env.SANITY_API_TOKEN)) {
+  if (!isValidRequest(req, previewSecret)) {
     return res.status(401).json({
       success: false,
-      message: 'Invalid signature',
+      message: 'Invalid secret',
     })
   }
 
