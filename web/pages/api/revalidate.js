51f31d328663b8a708d74705130d3fd55c873e22
andrew.eatherington <andrew.eatherington@cancer.org.uk>
2022-03-10 09:46:13 +0000

    revalidate individual pages - hopefully

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -1,9 +1,11 @@
 import { isValidRequest } from '@sanity/webhook'
 
+//const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
+
 const previewSecret='sk0XLbAz0MP6TVjACuu0HFOnNotHio0YFgVt1gq1jRCBfTRJji1bIBJ5j0oQURvSpwMbzeyDmG77FX6BPomLv6RJ6skMtkhTal2kUqJ2dK8bhjIFVqboGAOQULHa6qexwHFpSDVaaSI2neGKGMmia0oCPyA6jIoAzklNq2dBrseOXTpR3KJu'
 
-
 const revalidate = async (req, res) => {
+
   if (!isValidRequest(req, previewSecret)) {
     return res.status(401).json({
       success: false,
@@ -11,10 +13,12 @@ const revalidate = async (req, res) => {
     })
   }
 
+  const page_uid = req.body.uid.current
+
   try {
     // Customise the pages you need to revalidate here.
     // Keep the is simple for now
-    await res.unstable_revalidate('/our-research')
+    await res.unstable_revalidate(`/${page_uid}`)
 
     res.json({
       success: true,
